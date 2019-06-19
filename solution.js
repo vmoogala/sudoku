c8d2f683751ba230a7552a029c6b03cb1a090262
Vamshi Krishna Moogala <v.m@dal.ca>
2019-06-18 23:31:13 -0400

    Added a mechanism to check the occurrence of each digit in the sudoku

--- solution.js
+++ solution.js
@@ -3,6 +3,21 @@ const data = require("./question.js");
 // To track which numbers are left to fill in sudoku
 let remainingNumbers = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
+// To track the number of spaces left to fill by each number
+// When this is "1", fill the last available space
+// When this is "0" remove the current number from "remainingNumbers"
+let numberCount = {
+  "1": 9,
+  "2": 9,
+  "3": 9,
+  "4": 9,
+  "5": 9,
+  "6": 9,
+  "7": 9,
+  "8": 9,
+  "9": 9
+};
+
 const checkInRow = (rowNum, num) => {
   // Should check and fill numbers in a specific row
 };
@@ -26,3 +41,35 @@ const ifNumberExistsInColumn = (colNum, num) => {
 const ifNumberExistsInGrid = (gridNum, num) => {
   // Should return true if number exists in a specific grid
 };
+
+const fillNumberInSudoku = (num, rowNum, colNum) => {
+  // Fill the number
+  // Reduce the count for the number
+  // If the count is 1, invoke logic to fill the last available space
+  // If the count is 0, remove it from remaining numbers
+};
+
+const initializeNumberCount = data => {
+  // This will initialize the number count based on the given question
+
+  // Adding 0 to the numberCount to avoid checking in the below
+  // loop whether the number is "0" or actually a number
+  // This key will be deleted once the operation is done
+
+  numberCount["0"] = 81;
+
+  data.forEach(row => {
+    row.forEach(num => {
+      numberCount[num] = numberCount[num] - 1;
+    });
+  });
+
+  delete numberCount["0"];
+};
+
+const solveSudoku = data => {
+  initializeNumberCount(data);
+  console.log(numberCount);
+};
+
+solveSudoku(data);
