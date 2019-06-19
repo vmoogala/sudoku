836d2e105f6b532d1e32fb56fc15601eb2be88d7
Vamshi Krishna Moogala <v.m@dal.ca>
2019-06-18 22:56:55 -0400

    Idea for solution

---
+++ solution.js
@@ -0,0 +1,28 @@
+const data = require("./question.js");
+
+// To track which numbers are left to fill in sudoku
+let remainingNumbers = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+const checkInRow = (rowNum, num) => {
+  // Should check and fill numbers in a specific row
+};
+
+const checkInColumn = (colNum, num) => {
+  // Should check and fill numbers in a specific column
+};
+
+const checkInGrid = (gridNum, num) => {
+  // Should check and fill numbers in a specific grid
+};
+
+const ifNumberExistsInRow = (rowNum, num) => {
+  // Should return true if number exists in a specific row
+};
+
+const ifNumberExistsInColumn = (colNum, num) => {
+  // Should return true if number exists in a specific column
+};
+
+const ifNumberExistsInGrid = (gridNum, num) => {
+  // Should return true if number exists in a specific grid
+};
