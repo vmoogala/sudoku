b0d0703af56aa51b7ed7224f7d8d4bd7d195f0b1
Vamshi Krishna Moogala <v.m@dal.ca>
2019-06-23 19:46:28 -0400

    Filling numbers in grids

--- solution.js
+++ solution.js
@@ -169,21 +169,30 @@ const fillNumberInSudoku = (num, rowNum, colNum) => {
   gridCount[currentGridNum]--;
 
   if (numberCount[num] === 1) {
-    //fill the remaining number
+    checkEverywhereForANumber(num);
   } else if (numberCount[num] === 0) {
     remainingNumbers.delete(num);
   }
 
   if (rowCount[rowNum] == 1) {
     //fill the remaining number in that row
+    remainingNumbers.forEach(num => {
+      checkInRow(rowNum, num);
+    });
   }
 
   if (columnCount[colNum] == 1) {
     //fill the remaining number in that column
+    remainingNumbers.forEach(num => {
+      checkInColumn(colNum, num);
+    });
   }
 
   if (gridCount[currentGridNum] == 1) {
     //remove the remaining number in the grid
+    remainingNumbers.forEach(num => {
+      checkInGrid(currentGridNum, num);
+    });
   }
 
   console.log(
@@ -254,6 +263,7 @@ const solveSudoku = data => {
   initializeSetup(data);
   checkAllRowsForRemainingNumbers();
   checkAllColumnsForRemainingNumbers();
+  checkAllGridsForRemainingNumbers();
 };
 
 const getPossibleGridNumbersFromRowNumber = rowNum => {
@@ -276,14 +286,28 @@ const getPossibleGridNumbersFromColumnNumber = colNum => {
   }
 };
 
-const getPossibleRowNumbersFromGridNumber = gridNum => {
-  if ([0, 3, 6].includes(gridNum)) {
+const helper1 = num => {
+  if ([0, 3, 6].includes(num)) {
     return [0, 1, 2];
-  } else if ([1, 4, 7].includes(gridNum)) {
+  } else if ([1, 4, 7].includes(num)) {
     return [3, 4, 5];
-  } else if ([2, 5, 8].includes(gridNum)) {
+  } else if ([2, 5, 8].includes(num)) {
     return [6, 7, 8];
   }
+};
+
+const helper2 = num => {
+  if ([0, 3, 6].includes(num)) {
+    return [0, 3, 6];
+  } else if ([1, 4, 7].includes(num)) {
+    return [1, 4, 7];
+  } else if ([2, 5, 8].includes(num)) {
+    return [2, 5, 8];
+  }
+};
+
+const getPossibleRowNumbersFromGridNumber = gridNum => {
+  return helper1(gridNum);
 };
 
 const getPossibleColumnNumbersFromGridNumber = gridNum => {
@@ -294,6 +318,14 @@ const getPossibleColumnNumbersFromGridNumber = gridNum => {
   } else if (gridNum < 9) {
     return [6, 7, 8];
   }
+};
+
+const getNumbersToRemoveBasedOnRow = rowNum => {
+  return helper1(rowNum);
+};
+
+const getNumbersToRemoveBasedOnColumn = colNum => {
+  return helper2(colNum);
 };
 
 const getEmptyPositionsFromRow = rowNum => {
@@ -314,6 +346,50 @@ const getEmptyPositionsFromColumn = colNum => {
     }
   });
   return arr;
+};
+
+const getEmptyPositionsFromGrid = gridNum => {
+  let arr = [];
+  getGridValues(gridNum).forEach((d, i) => {
+    if (d === 0) {
+      arr.push(i);
+    }
+  });
+  return arr;
+};
+
+const getPossibleRowsAndColumnsFromGridNumber = gridNum => {
+  return [
+    getPossibleColumnNumbersFromGridNumber(gridNum),
+    getPossibleRowNumbersFromGridNumber(gridNum)
+  ];
+};
+
+const getRowAndColumnNumFromGridNumAndPosition = (
+  gridNum,
+  posNum,
+  possRows,
+  possCols
+) => {
+  let row;
+  let col;
+  if ([0, 1, 2].includes(posNum)) {
+    row = possRows[0];
+  } else if ([3, 4, 5].includes(posNum)) {
+    row = possRows[1];
+  } else if ([6, 7, 8].includes(posNum)) {
+    row = possRows[2];
+  }
+
+  if ([0, 3, 6].includes(posNum)) {
+    col = possCols[0];
+  } else if ([1, 4, 7].includes(posNum)) {
+    col = possCols[1];
+  } else if ([2, 5, 8].includes(posNum)) {
+    col = possCols[2];
+  }
+
+  return [row, col];
 };
 
 function removeElementsFromArray(arr, elements) {
@@ -411,9 +487,47 @@ const checkInColumn = (colNum, num) => {
  * @param {*} gridNum
  * @param {*} num
  */
-const checkInGrid = (gridNum, num) => {};
+const checkInGrid = (gridNum, num) => {
+  if (!ifNumberExistsInGrid(gridNum, num)) {
+    let emptyPositions = getEmptyPositionsFromGrid(gridNum);
+    let [
+      possibleRows,
+      possibleColumns
+    ] = getPossibleRowsAndColumnsFromGridNumber(gridNum);
+
+    possibleRows.forEach(row => {
+      if (ifNumberExistsInRow(row, num)) {
+        emptyPositions = removeElementsFromArray(
+          emptyPositions,
+          getNumbersToRemoveBasedOnRow(row)
+        );
+      }
+    });
+
+    possibleColumns.forEach(col => {
+      if (ifNumberExistsInColumn(col, num)) {
+        emptyPositions = removeElementsFromArray(
+          emptyPositions,
+          getNumbersToRemoveBasedOnColumn(col)
+        );
+      }
+    });
+
+    if (emptyPositions.length == 1) {
+      let [row, col] = getRowAndColumnNumFromGridNumAndPosition(
+        gridNum,
+        emptyPositions[0],
+        possibleRows,
+        possibleColumns
+      );
+
+      fillNumberInSudoku(num, row, col);
+    }
+  }
+};
 
 const checkAllRowsForRemainingNumbers = () => {
+  console.log("\nchecking all rows");
   remainingNumbers.forEach(num => {
     remainingRows.forEach(rowNum => {
       checkInRow(rowNum, num);
@@ -422,10 +536,32 @@ const checkAllRowsForRemainingNumbers = () => {
 };
 
 const checkAllColumnsForRemainingNumbers = () => {
+  console.log("\nchecking all columns");
   remainingNumbers.forEach(num => {
     remainingColumns.forEach(colNum => {
       checkInColumn(colNum, num);
     });
+  });
+};
+
+const checkAllGridsForRemainingNumbers = () => {
+  console.log("\nchecking all grids");
+  remainingNumbers.forEach(num => {
+    remainingGrids.forEach(gridNum => {
+      checkInGrid(gridNum, num);
+    });
+  });
+};
+
+const checkEverywhereForANumber = num => {
+  remainingRows.forEach(row => {
+    checkInRow(row, num);
+  });
+  remainingColumns.forEach(col => {
+    checkInRow(col, num);
+  });
+  remainingGrids.forEach(grid => {
+    checkInGrid(grid, num);
   });
 };
 
